dd74101cdc96a612bc7a73946872cb23ccffa17f
tessadenatris <32036459+tessadenatris@users.noreply.github.com>
2020-02-23 16:56:06 +0100

    Website aangepast.
    Snoozy html pagina toegevoegd.

--- website kopie 2/Java/portfolio.js
+++ website kopie 2/Java/portfolio.js
@@ -6,3 +6,20 @@ function openMenu(){
 }
 
 burger.addEventListener('click', openMenu);
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  for (i = 0; i < slides.length; i++) { // loopt door alle plaatjes van de array
+    slides[i].style.display = "none"; // zorgt dat alle plaatjes niet zichtbaar zijn 
+  }
+  slideIndex++; 
+  if (slideIndex > slides.length) {slideIndex = 1} //gaat terug naar eerste item in array als je over de lengte van de array gaat   
+  
+  slides[slideIndex-1].style.display = "block"; //laat 1 item van de array zien 
+                                                               
+  setTimeout(showSlides, 5200); // Change image every seconds
+}
